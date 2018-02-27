46085de30b424912248095fafb8ad055e3fa93a4
Nafeesa Nargis <nafeesa.nargis@brillio.com>
2018-02-27 11:25:43 +0530

    added verizon use case

--- index.js
+++ index.js
@@ -26,36 +26,69 @@ alexaApp.express({
 // from here on you can setup any other express routes or middlewares as normal
 app.set("view engine", "ejs");
 
-alexaApp.launch(function(request, response) {
+alexaApp.launch(function (request, response) {
   response.say("You launched the app!");
 });
 
 //alexaApp.dictionary = { "names": ["matt", "joe", "bob", "bill", "mary", "jane", "dawn"] };
 
 alexaApp.intent("PlanMyTrip", {
-    "slots": [
-      {
-        "name": "fromCity",
-        "type": "AMAZON.US_CITY",
-        "value": "Seattle"
-      },
-      {
-        "name": "toCity",
-        "type": "AMAZON.US_CITY",
-        "value": "Portland"
-      },
-      {
-        "name": "travelDate",
-        "type": "AMAZON.DATE"
-      }
-    ],
-    "utterances": [
-      "i want to travel from {fromCity} to {toCity} {travelDate}"
-    ]
-  },
-  function(request, response) {
+  "slots": [
+    {
+      "name": "fromCity",
+      "type": "AMAZON.US_CITY",
+      "value": "Seattle"
+    },
+    {
+      "name": "toCity",
+      "type": "AMAZON.US_CITY",
+      "value": "Portland"
+    },
+    {
+      "name": "travelDate",
+      "type": "AMAZON.DATE"
+    }
+  ],
+  "utterances": [
+    "i want to travel from {fromCity} to {toCity} {travelDate}"
+  ]
+},
+  function (request, response) {
+    response.say("Success!");
+  }
+);
+
+alexaApp.intent("PlanMyTrip", {
+  "slots": [
+    {
+      "name": "companyName",
+      "type": "AMAZON.Corporation",
+      "value": ["Verizon", "Brillio"]
+    },
+    {
+      "name": "botName",
+      "type": "AMAZON.DE_FIRST_NAME",
+      "value": "Hella"
+    },
+    {
+      "name": "newDate",
+      "type": "AMAZON.DATE"
+    }
+  ],
+  "utterances": [
+    "open {companyName} NOC Dashboard",
+    "Hi {botName}",
+    "what are the number of pipeline orders we have",
+    "what are the number of current orders due for {newDate}",
+    "what are the future orders that are due",
+    "which are the past orders that are due as of {newDate}",
+    "what are the key metrics for the NOC in this month"
+  ]
+},
+  function (request, response) {
     response.say("Success!");
   }
 );
 
 app.listen(PORT, () => console.log("Listening on port " + PORT + "."));
+
