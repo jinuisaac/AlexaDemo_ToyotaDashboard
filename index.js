21c5206d4d55fd9539ab2c4dd7393c0319219e9e
Nafeesa Nargis <nafeesa.nargis@brillio.com>
2018-04-11 13:07:17 +0530

    updated QA

--- index.js
+++ index.js
@@ -40,31 +40,49 @@ alexaApp.intent("Intro_question", {
     }
   ],
   "utterances": [
-    "Hi - How is the pitch dashboard looking {newDate}?"
+    "Hello..how does the Pitch dashboard look {newDate}?"
   ]
 },
   function (request, response) {
     request.getSession().set();
-    response.say("The Dashboard looks mostly clear but for 2 Incidents, One at Kansas City and Second one at Des Moines.");
+    response.say("Hello... The Pitch dashboard looks healthy but for 2 Incidents at Kansa City & Des Monies locations.");
     response.shouldEndSession(false);
   }
 );
 
-alexaApp.intent("Details_ongoing_incident", {
+alexaApp.intent("Details_firstCity_incident", {
   "slots": [
     {
       "name": "city",
-      "type": "AMAZON.US_CITY"
+      "type": "AMAZON.AT_CITY"
     }
   ],
   "utterances": [
-    "Hi Can you please provide me some more details around the ongoing incident at {city} location."
+    "Can you please provide me more details around {city} Incident?"
   ]
 },
   function (request, response) {
     console.log(response);
     request.getSession().set();
-    response.say("Sure. There is a network outage at Kansas City location Impacting : WMS applications. The Critical P1 L000010 has been raised and Incident Manager Swarn is currently co-ordinating with all the support teams.");
+    response.say("There is an ongoing incident since 4/19/2017 2:22:15 PM impacting All Warehouse knowledge workers within Kansas City due to MPLS, Internet & 4G outage.");
+    response.shouldEndSession(false);
+  }
+);
+
+alexaApp.intent("Incident_manager", {
+  "slots": [
+    {
+      "name": "city",
+      "type": "AMAZON.AT_CITY"
+    }
+  ],
+  "utterances": [
+    "Who is the Incident Manager for this {city} Incident?"
+  ]
+},
+  function (request, response) {
+    request.getSession().set();
+    response.say("Chethan Krishna is the Incident Manger");
     response.shouldEndSession(false);
   }
 );
@@ -76,20 +94,145 @@ alexaApp.intent("Latest_update", {
 },
   function (request, response) {
     request.getSession().set();
-    response.say("Network Team to raise high priority tickets with DSR to get 4G functional at the earliest.");
+    response.say("MPLS & Internet outage is going on at the site. Both Service Provider devices are down due to an area wide outage. 4G is not operational & a high priority case has been raised with DSR");
+    response.shouldEndSession(false);
+  }
+);
+
+alexaApp.intent("Previous_instance", {
+  "utterances": [
+    "Is there a previous instance of such incident?"
+  ]
+},
+  function (request, response) {
+    request.getSession().set();
+    response.say("No there are no such instances in the past reported");
+    response.shouldEndSession(false);
+  }
+);
+
+alexaApp.intent("Conference_bridge", {
+  "utterances": [
+    "Is the conference Bridge Still Open?"
+  ]
+},
+  function (request, response) {
+    request.getSession().set();
+    response.say("Yes the bridge is still running at +1 213-296-6290");
     response.shouldEndSession(false);
   }
 );
 
 alexaApp.intent("Escalate", {
   "utterances": [
-    "Do you need me to escalate this incident or call the incident manager?"
+    "Does this need to be escalated?"
   ]
 },
   function (request, response) {
     request.getSession().set();
-    response.say("NO, i think the incident is under control.");
-    //If Yes => call Swarn.....
+    response.say("No the team is working towards resolving this and is Expected resolution time is 4:22:15 PM");
+    response.shouldEndSession(false);
+  }
+);
+
+alexaApp.intent("Change_implement", {
+  "utterances": [
+    "Was there any recent change implemented at this location?"
+  ]
+},
+  function (request, response) {
+    request.getSession().set();
+    response.say("There are no details available around changes implemented at this location for last 6 months.");
+    response.shouldEndSession(false);
+  }
+);
+
+alexaApp.intent("Second_incident", {
+  "slots": [
+    {
+      "name": "city",
+      "type": "AMAZON.AT_CITY"
+    }
+  ],
+  "utterances": [
+    "Ok thank you. Can you please provide me more inputs around {city} Incident?"
+  ]
+},
+  function (request, response) {
+    request.getSession().set();
+    response.say("Sure. MPLS, Internet & 4G outage at Des Moines affecting WMS");
+    response.shouldEndSession(false);
+  }
+);
+
+alexaApp.intent("Incident_number", {
+  "utterances": [
+    "Can you provide me the Incident Number and impacted users?"
+  ]
+},
+  function (request, response) {
+    request.getSession().set();
+    response.say("The Incident # L000010 and is impacting the Complete Warehouse Operations");
+    response.shouldEndSession(false);
+  }
+);
+
+alexaApp.intent("Previous_instance", {
+  "utterances": [
+    "Is there a previous instance of such incident?"
+  ]
+},
+  function (request, response) {
+    request.getSession().set();
+    response.say("No there are no such instances in the past reported");
+    response.shouldEndSession(false);
+  }
+);
+
+alexaApp.intent("Change_implement", {
+  "utterances": [
+    "Was there any recent change implemented at this location?"
+  ]
+},
+  function (request, response) {
+    request.getSession().set();
+    response.say("Yes there was a change implemented 2 weeks back on one of the router configurations, which was reported to be a successful change.");
+    response.shouldEndSession(false);
+  }
+);
+
+alexaApp.intent("Next_steps", {
+  "utterances": [
+    "What are the next steps?"
+  ]
+},
+  function (request, response) {
+    request.getSession().set();
+    response.say("Network Team to raise high priority tickets with DSR to get 4G functional at the earliest");
+    response.shouldEndSession(false);
+  }
+);
+
+alexaApp.intent("Escalate", {
+  "utterances": [
+    "Does this incident need escalation?"
+  ]
+},
+  function (request, response) {
+    request.getSession().set();
+    response.say("No, the team is already following up with relevant stakeholders and are receiving the required updates to proceed further");
+    response.shouldEndSession(false);
+  }
+);
+
+alexaApp.intent("Other_details", {
+  "utterances": [
+    "Are there any other important details you have for me?"
+  ]
+},
+  function (request, response) {
+    request.getSession().set();
+    response.say("Not at this juncture. Thanks.");
     response.shouldEndSession(false);
   }
 );
@@ -102,18 +245,6 @@ alexaApp.intent("Say_thanks", {
   function (request, response) {
     request.getSession().set();
     response.say("You are welcome.");
-    response.shouldEndSession(false);
-  }
-);
-
-alexaApp.intent("Second_incident", {
-  "utterances": [
-    "Hi Can you please read me the second incident?"
-  ]
-},
-  function (request, response) {
-    request.getSession().set();
-    response.say("Sure. L000040 - MPLS, Internet & 4G outage at Des Moines affecting WMS.");
     response.shouldEndSession(false);
   }
 );
